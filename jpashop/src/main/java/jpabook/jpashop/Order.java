2bb6a0ef1ea248e434f06f5150bf4c84d6ce6aae
minkj1992 <minkj1992@gmail.com>
2020-03-14 12:15:49 +0900

    [spring-boot-jpabook-02] Order Entity 생성

--- jpashop/src/main/java/jpabook/jpashop/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/Order.java
@@ -26,12 +26,12 @@ public class Order {
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status; //[ORDER, CANCEL]
-
-
+    
 }
